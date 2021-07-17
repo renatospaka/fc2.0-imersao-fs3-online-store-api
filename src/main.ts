7a5a4707d72a16c8ea62d46cdc8e98b00593257f
Renato Spakauskas <renatospaka@gmail.com>
2021-07-17 13:52:02 -0300

    feat: :sparkles: 404 interception filter
    an interception filter was creasted to intercept every time a parameter doesn't return values from the database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EntityNotFoundExceptionFilter } from './exceptionFilters/entityNotFound.exceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new EntityNotFoundExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
