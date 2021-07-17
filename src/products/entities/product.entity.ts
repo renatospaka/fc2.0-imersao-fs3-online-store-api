b5a2bdbf5e24b5e01da607e9b65a55b78ef43aa4
Renato Spakauskas <renatospaka@gmail.com>
2021-07-17 11:57:06 -0300

    feat: :sparkles: product entity
    the products entity along with postgres table and typeorm ready to go.
    
    next steps: implement the logic and business rules for the product entity and all related objects

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "products" })
+export class Product {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image_url: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  slug: string;
+
+  @CreateDateColumn( {type: "timestamp" })
+  created_at: Date;
+}
